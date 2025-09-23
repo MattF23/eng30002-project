ea845774e34fa89ae1dd1d54d2e05a9e7436ca55
matt01 <matthewf789@gmail.com>
2025-09-23 16:59:23 +1000

    added playing sound library.

--- warn.py
+++ warn.py
@@ -1,13 +1,31 @@
-import time
+import pygame.mixer as mixer
+import pygame.time
 
 def warn(warnings):
     """
     Function that will warn the driver of impending doom.
-    TODO
+    Plays a different sound file based on the amount of warning
     """
+
+    mixer.init()
+
     if warnings == 1:
-        print("Warning!")
+        sound = mixer.Sound("path/to/file")
+        playing = sound.play()
+
+        while(playing.get_busy()):
+            pygame.time.delay(100)
+
     elif warnings == 2:
-        print("Warning!!")
+        sound = mixer.Sound("path/to/file")
+        playing = sound.play()
+
+        while(playing.get_busy()):
+            pygame.time.delay(100)
+
     else:
-        print("Warning!!!")
+        sound = mixer.Sound("path/to/file")
+        playing = sound.play()
+
+        while(playing.get_busy()):
+            pygame.time.delay(100)
