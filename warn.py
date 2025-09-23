4e4f9d8d2716c6a430ee0940c0d1a59139cd4225
matt01 <matthewf789@gmail.com>
2025-09-23 17:32:03 +1000

    Added audio files

--- warn.py
+++ warn.py
@@ -10,21 +10,21 @@ def warn(warnings):
     mixer.init()
 
     if warnings == 1:
-        sound = mixer.Sound("path/to/file")
+        sound = mixer.Sound("audio/warning1.mp3")
         playing = sound.play()
 
         while(playing.get_busy()):
             pygame.time.delay(100)
 
     elif warnings == 2:
-        sound = mixer.Sound("path/to/file")
+        sound = mixer.Sound("audio/warning2.mp3")
         playing = sound.play()
 
         while(playing.get_busy()):
             pygame.time.delay(100)
 
     else:
-        sound = mixer.Sound("path/to/file")
+        sound = mixer.Sound("audio/warning3.mp3")
         playing = sound.play()
 
         while(playing.get_busy()):
