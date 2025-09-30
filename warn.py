731f5773c79a1a78d31be44fc473e6ce1492c371
matt01 <matthewf789@gmail.com>
2025-09-30 17:58:52 +1000

    Thread safe?

--- warn.py
+++ warn.py
@@ -1,4 +1,6 @@
-from buzzer import play_tone
+#from buzzer import play_tone
+from gpiozero import PWMOutputDevice
+from time import sleep
 
 def warn(warnings = 1):
     """
@@ -6,6 +8,20 @@ def warn(warnings = 1):
     Buzzes the buzzer a different amount of time based on the level of warning
     """
     #Implementation
+
+    def play_tone(frequency, duration):
+        buzzer = PWMOutputDevice(18)
+
+        """Play a tone on the buzzer."""
+        print("Buzzing!")
+        if frequency == 0:
+            # Pause (no sound)
+            sleep(duration)
+            return
+        buzzer.frequency = frequency
+        buzzer.value = 0.5 # 50% duty cycle
+        sleep(duration)
+        buzzer.value = 0 # stop tone
 
     if warnings ==  1:
         play_tone(261, 500)
